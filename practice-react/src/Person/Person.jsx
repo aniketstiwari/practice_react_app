35d3379160c7fc803f372b99078583ccaf1aeff4
aniketstiwari <anikaittiwari91@gmail.com>
2021-03-24 10:42:24 +0530

    [35] Outputting Dynamic content

--- practice-react/src/Person/Person.jsx
+++ practice-react/src/Person/Person.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const person = () => {
-    return <p> Hi I am learning React</p>
+    return <p> Hi I am aniket and I am {Math.floor(Math.random() * 30)} years old</p>
 }
 
 export default person;

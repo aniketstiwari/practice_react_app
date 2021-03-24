36bfb78c16a8ef6c9f748057e000ab45eeb81006
aniketstiwari <anikaittiwari91@gmail.com>
2021-03-24 10:57:08 +0530

    [37] Understanding the children prop

--- practice-react/src/App.js
+++ practice-react/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <h1>HI I am React App</h1>
         <p>This is really working</p>
         <Person name="Max" age="28"/>
-        <Person name="Manu" age="29">Ny Hobies=: Racing</Person>
+        <Person name="Manu" age="29">My Hobbies: Racing</Person>
         <Person name="stephanie" age="30"/>
       </div>
     )

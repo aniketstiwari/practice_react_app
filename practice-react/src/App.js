63f129a7d0e8f95df85cd8556761f33ff8bc7697
aniketstiwari <anikaittiwari91@gmail.com>
2021-03-25 18:47:25 +0530

    [68] Adding and using radium

--- practice-react/src/App.js
+++ practice-react/src/App.js
@@ -2,6 +2,7 @@ import React , { Component } from 'react';
 //import React , { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import Radium from 'radium';
 
 //props are set and pass from outside. State is manage from inside the component
 
@@ -136,7 +137,11 @@ class App extends Component {
       font: "inherit",
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -171,6 +176,10 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     let classes = [];
@@ -199,4 +208,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);

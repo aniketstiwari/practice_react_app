c5ff472206848228a29f327123b2614bf608d9c4
aniketstiwari <anikaittiwari91@gmail.com>
2021-03-25 14:08:06 +0530

    [54] Handling dynamic content javascript wau

--- practice-react/src/App.js
+++ practice-react/src/App.js
@@ -117,6 +117,29 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+    if(this.state.showPersons) {
+      persons = (
+        <div>
+          <Person 
+            name={this.state.persons[0].name} 
+            age={this.state.persons[0].age}
+          />
+          <Person 
+            name={this.state.persons[1].name} 
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'max')}
+            changed={this.nameChangeHandler}>
+              My Hobbies: Racing
+          </Person>
+          <Person 
+            name={this.state.persons[2].name} 
+            age={this.state.persons[2].age}
+          />
+        </div>
+      )
+    }
+
     return (
       <div className="App">
         <h1>HI I am React App</h1>
@@ -129,26 +152,7 @@ class App extends Component {
         {/*one other way of passing arguments
         onClick={() => this.switchNameHandler('maximilian!!')}
         */}
-        { 
-        this.state.showPersons ? 
-          <div>
-            <Person 
-              name={this.state.persons[0].name} 
-              age={this.state.persons[0].age}
-            />
-            <Person 
-              name={this.state.persons[1].name} 
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, 'max')}
-              changed={this.nameChangeHandler}>
-                My Hobbies: Racing
-            </Person>
-            <Person 
-              name={this.state.persons[2].name} 
-              age={this.state.persons[2].age}
-            />
-          </div> : null 
-        }
+        {persons}
       </div>
     )
   }

c9854c0e6c14f532dd8102022336d658f6cd60e7
aniketstiwari <anikaittiwari91@gmail.com>
2021-03-25 14:31:40 +0530

    [56] Outputting list

--- practice-react/src/App.js
+++ practice-react/src/App.js
@@ -121,7 +121,13 @@ class App extends Component {
     if(this.state.showPersons) {
       persons = (
         <div>
-          <Person 
+          {this.state.persons.map((person, index) => {
+            return <Person
+              name={person.name}
+              age={person.age}
+            />
+          })}
+          {/* <Person 
             name={this.state.persons[0].name} 
             age={this.state.persons[0].age}
           />
@@ -135,7 +141,7 @@ class App extends Component {
           <Person 
             name={this.state.persons[2].name} 
             age={this.state.persons[2].age}
-          />
+          /> */}
         </div>
       )
     }

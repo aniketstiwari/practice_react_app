bb334bc5a9bab5a92505b6f09c32cf15ad5d1c0b
aniketstiwari <anikaittiwari91@gmail.com>
2021-03-28 16:56:28 +0530

    [97] Using shouldComponentUpdate for optimization

--- practice-react/src/components/Persons/Persons.js
+++ practice-react/src/components/Persons/Persons.js
@@ -28,7 +28,14 @@ class Persons extends Component {
 
   shouldComponentUpdate(nextProps, nextState) {
     console.log('[Persons.js] shouldComponentUpdate');
-    return true;
+    //here we are comparing if there is no change in the persons then we shouldn't run
+    //persons hooks
+    if(nextProps.persons !== this.props.persons){
+      return true;
+    }else {
+      return false;
+    }
+    
   }
 
   getSnapshotBeforeUpdate(prevProps, prevState) {

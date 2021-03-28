b786bba88a718f1ff131cb5ac04402e80323ee29
aniketstiwari <anikaittiwari91@gmail.com>
2021-03-28 14:55:41 +0530

    [91] Component update lifecycle for props changes

--- practice-react/src/components/Persons/Person/Person.jsx
+++ practice-react/src/components/Persons/Person/Person.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Person.module.css';
 // import Radium from 'radium';
 //import styled from 'styled-components';
@@ -16,31 +16,44 @@ import classes from './Person.module.css';
 //   }
 // `;
 
-const person = (props) => {
-  // const style = {
-  //   '@media (min-width: 500px)': {
-  //     width: '450px'
-  //   }
-  // }
-  // const rnd = Math.random();
-  // if (rnd > 0.7) {
-  //   throw new Error("Something went Wrong");
-  // }
-  console.log('Person.js rendering');
-  return (
-    // <div className="Person" style={style}>
-    // <StyleDiv>
-    //   <p onClick={props.click}> Hi I am {props.name} and I am {props.age} years old</p>
-    //   <p>{props.children}</p>
-    //   <input type="text" onChange={props.changed} value={props.name} />
-    // </StyleDiv>
-    <div className={classes.Person}>
-      <p onClick={props.click}> Hi I am {props.name} and I am {props.age} years old</p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
-    </div>
-  )
+// const person = (props) => {
+//   // const style = {
+//   //   '@media (min-width: 500px)': {
+//   //     width: '450px'
+//   //   }
+//   // }
+//   // const rnd = Math.random();
+//   // if (rnd > 0.7) {
+//   //   throw new Error("Something went Wrong");
+//   // }
+//   console.log('Person.js rendering');
+//   return (
+//     // <div className="Person" style={style}>
+//     // <StyleDiv>
+//     //   <p onClick={props.click}> Hi I am {props.name} and I am {props.age} years old</p>
+//     //   <p>{props.children}</p>
+//     //   <input type="text" onChange={props.changed} value={props.name} />
+//     // </StyleDiv>
+//     <div className={classes.Person}>
+//       <p onClick={props.click}> Hi I am {props.name} and I am {props.age} years old</p>
+//       <p>{props.children}</p>
+//       <input type="text" onChange={props.changed} value={props.name} />
+//     </div>
+//   )
+// }
+
+class Person extends Component {
+  render() {
+    console.log('Person.js rendering');
+    return (  
+      <div className={classes.Person}>
+        <p onClick={this.props.click}> Hi I am {this.props.name} and I am {this.props.age} years old</p>
+        <p>{this.props.children}</p>
+        <input type="text" onChange={this.props.changed} value={this.props.name} />
+      </div>
+    )
+  }
 }
 
 //export default Radium(person);
-export default person;
+export default Person;
--- practice-react/src/components/Persons/Persons.js
+++ practice-react/src/components/Persons/Persons.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
 //If you want to omit the return statement then start the open bracket
@@ -9,16 +9,49 @@ import Person from './Person/Person';
 
 //Also If  you return jsx then you can omit the brackers () too
 
-const persons = (props) => {
-  console.log('Persons.js rendering');
-  return props.persons.map((person, index) => {
-    return <Person key={person.id}
-          name={person.name}
-          age={person.age}
-          click={() => props.clicked(index)}
-          changed={(event) => props.changed(event, person.id)}
-        />
-  });
-};
+// const persons = (props) => {
+//   console.log('Persons.js rendering');
+//   return props.persons.map((person, index) => {
+//     return <Person key={person.id}
+//           name={person.name}
+//           age={person.age}
+//           click={() => props.clicked(index)}
+//           changed={(event) => props.changed(event, person.id)}
+//         />
+//   });
+// };
 
-export default persons;
+class Persons extends Component {
+  // static getDerivedStateFromProps(props, state) {
+  //   console.log('[Persons.js] getDerivedStateFromProps')
+  // }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[Persons.js] shouldComponentUpdate');
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('[Persons.js] getSnapshotBeforeUpdate');
+    return { message: 'Snapshot!'};
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('[Persons.js] componentDidUpdate');
+    console.log(snapshot);
+  }
+
+  render() {
+    console.log('Persons.js rendering');
+    return this.props.persons.map((person, index) => {
+      return <Person key={person.id}
+            name={person.name}
+            age={person.age}
+            click={() => this.props.clicked(index)}
+            changed={(event) => this.props.changed(event, person.id)}
+          />
+    });
+  }
+}
+
+export default Persons;
